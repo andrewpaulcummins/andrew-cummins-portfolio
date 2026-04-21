d782f300abd17697b1d717f407e121ed5b4a6735
andrewpaulcummins <45366250+andrewpaulcummins@users.noreply.github.com>
2026-04-21 18:20:40 +0100

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/andrew-cummins-portfolio/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
